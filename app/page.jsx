aa1444ce5cca47f238606c5355c37b26f617efa1
mileslathan <mileslathan.cn@gmail.com>
2023-06-20 16:28:54 -0400

    Fixed the layout issues and root folders.

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+      <section className="w-full flex-center flex-col">
+        <h1 className="text-5xl font-bold">Miles Portfolio</h1>
+      </section>
+  )
+}
+
+export default Home
---
+++ components/Nav.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+import Link from 'next/link'
+
+const Nav = () => {
+  return (
+    <div>This is Nav Bar.</div>
+  )
+}
+
+export default Nav
