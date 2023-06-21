9ebaf254d7563cc79fdf490050877400aa6206e0
mileslathan <mileslathan.cn@gmail.com>
2023-06-21 12:40:29 -0400

    Getting page to render proper items aligned correctly.

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,41 @@
-
+import Image from 'next/image'
 
 const Home = () => {
   return (
-      <section className="w-full flex-center flex-col">
-        <h1 className="text-5xl font-bold">Miles Portfolio</h1>
+      <section className="container">
+        {/* <table className="container">
+          <tr className="flex-start">
+                <td><h1 className="text-5xl poppins gap-2">Welcome to</h1>
+            <h1 className="text-5xl font-bold blue_gradient ml-2">Miles Portfolio.</h1>
+          <p className="poppins text-xl mt-2 flex-start">Hi I'm Miles, a passionate, self-driven software developer.</p>
+            </td>
+            <td rowspan="2"><Image src="/assets/images/profile.jpg"
+          width={400}
+          height={400}
+          className="rounded-full filter grayscale"
+          alt="Miles Profile Picture"
+          ></Image></td>
+            <tr className="mt-10"><h1>My Languages :</h1></tr>
+          </tr>
+        </table> */}
+        <div className="container">
+          <div>
+                <h1 className="text-5xl poppins gap-2">Welcome to</h1>
+            <h1 className="text-5xl font-bold blue_gradient pl-10">Miles Portfolio</h1>
+                {/* <h1 className="text-5xl poppins gap-2">.</h1> */}
+          <p className="poppins text-xl mt-2 flex-start max-w-lg">Hi I'm Miles, a passionate, self-driven software developer. Building dynamic Frontend and Backend applications.</p>
+          <div className="mt-10"><h1>My Languages :</h1>
+          next.js</div>
+          </div>
+          <div>
+          <Image src="/assets/images/profile.jpg"
+          width={400}
+          height={400}
+          className="rounded-full filter grayscale ml-5"
+          alt="Miles Profile Picture"
+          ></Image>
+          </div>
+        </div>
       </section>
   )
 }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,7 +4,27 @@ import Link from 'next/link'
 
 const Nav = () => {
   return (
-    <div>This is Nav Bar.</div>
+    <nav className="flex-between w-full mb-16 pt-3">
+        <Link href="/" className="flex-start gap-2">      
+        <h1 className="logo_tex poppins">M I L E S L A T H A N</h1>
+        </Link>
+        <div className="flex-end gap-5 poppins text-2xl">
+        <Link href="/"
+        className="black_btn poppins">
+        HOME
+        </Link>
+        <Link href="/about"
+        className="outline_btn poppins">
+        WHO I AM
+        </Link>
+        <Link href="/projects">
+        My Projects
+        </Link>
+        <Link href="/contact">
+        Contact Me
+        </Link>
+        </div>
+    </nav>
   )
 }
 
