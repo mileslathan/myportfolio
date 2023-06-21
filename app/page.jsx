21c1dd818038f8441d552a6834031f623f0e96c0
mileslathan <mileslathan.cn@gmail.com>
2023-06-21 14:44:32 -0400

    fixed the landing page with proper spacing and animation.

--- app/page.jsx
+++ app/page.jsx
@@ -18,20 +18,30 @@ const Home = () => {
             <tr className="mt-10"><h1>My Languages :</h1></tr>
           </tr>
         </table> */}
-        <div className="container">
-          <div>
-                <h1 className="text-5xl poppins gap-2">Welcome to</h1>
-            <h1 className="text-5xl font-bold blue_gradient pl-10">Miles Portfolio</h1>
+        <div className="container biryaniRegular">
+          <div className="max-w-xl">
+                <h1 className="text-6xl gap-2">WELCOME TO</h1>
+            <h1 className="text-6xl font-bold animate-character pt-3 pl-10">MILES' PORTFOLIO</h1>
                 {/* <h1 className="text-5xl poppins gap-2">.</h1> */}
-          <p className="poppins text-xl mt-2 flex-start max-w-lg">Hi I'm Miles, a passionate, self-driven software developer. Building dynamic Frontend and Backend applications.</p>
-          <div className="mt-10"><h1>My Languages :</h1>
-          next.js</div>
+          <p className="text-xl2 mt- flex-start">Hi I'm Miles, a passionate, self-driven software developer. Building dynamic Frontend and Backend applications.</p>
+          <div className="mt-10 text-xl2"><h1>My Languages :</h1>
+            <ul>
+              <li>JAVASCRIPT</li>
+              <li>NODEJS</li>
+              <li>EXPRESS</li>
+              <li>REACT</li>
+              <li>HTML/CSS</li>
+              <li>DJANGO</li>
+              <li>POSTGRES</li>
+              <li>MONGODB</li>
+              </ul>
+            </div>
           </div>
-          <div>
+          <div className="justify-content-end">
           <Image src="/assets/images/profile.jpg"
           width={400}
           height={400}
-          className="rounded-full filter grayscale ml-5"
+          className="rounded-full filter grayscale ml-50"
           alt="Miles Profile Picture"
           ></Image>
           </div>
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,24 +4,26 @@ import Link from 'next/link'
 
 const Nav = () => {
   return (
-    <nav className="flex-between w-full mb-16 pt-3">
+    <nav className="flex-between w-full mb-16 mt-4 pt-3">
+        <Link href="/"
+        className="biryaniRegular">
+        LOGO
+        </Link>
         <Link href="/" className="flex-start gap-2">      
-        <h1 className="logo_tex poppins">M I L E S L A T H A N</h1>
+        <h1 className="logo_tex poppins"></h1>
         </Link>
         <div className="flex-end gap-5 poppins text-2xl">
-        <Link href="/"
-        className="black_btn poppins">
-        HOME
-        </Link>
         <Link href="/about"
-        className="outline_btn poppins">
+        className="outline_btn biryaniRegular">
         WHO I AM
         </Link>
-        <Link href="/projects">
-        My Projects
+        <Link href="/projects"
+        className="outline_btn biryaniRegular">
+        MY PROJECTS
         </Link>
-        <Link href="/contact">
-        Contact Me
+        <Link href="/contact"
+        className="black_btn biryaniRegular">
+        CONTACT ME
         </Link>
         </div>
     </nav>
